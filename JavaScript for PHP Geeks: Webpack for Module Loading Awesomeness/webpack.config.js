a9d63ba016a027cca08e3c54768514d0b79c0f8a
Galen Senogles <galen.senogles@gmail.com>
2018-11-06 13:25:36 -0800

    Organize code to use webpack better

---
+++ JavaScript for PHP Geeks: Webpack for Module Loading Awesomeness/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+	entry: './web/assets/js/rep_log.js',
+	output: {
+		path: path.resolve(__dirname, 'web','build'),
+		filename: 'rep_log.js',
+	},
+    mode: "development" // (or "production")
+};
