bc4bf6b04e9d0a4e54b7406f86cd3ced012b9fac
Galen Senogles <galen.senogles@gmail.com>
2018-11-06 17:48:35 -0800

    Add copy-webpack-plugin for static images

--- JavaScript for PHP Geeks: Webpack for Module Loading Awesomeness/assets/js/layout.js
+++ JavaScript for PHP Geeks: Webpack for Module Loading Awesomeness/assets/js/layout.js
@@ -4,6 +4,7 @@ const $ = require('jquery');
 require('bootstrap');
 
 require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
 require('../css/main.css');
 
 require("@babel/polyfill");
--- JavaScript for PHP Geeks: Webpack for Module Loading Awesomeness/webpack.config.js
+++ JavaScript for PHP Geeks: Webpack for Module Loading Awesomeness/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry: {
-		rep_log: './web/assets/js/rep_log.js',
-		login: './web/assets/js/login.js',
-		layout: './web/assets/js/layout.js'
+		rep_log: './assets/js/rep_log.js',
+		login: './assets/js/login.js',
+		layout: './assets/js/layout.js'
 	},
 	output: {
 		path: path.resolve(__dirname, 'web','build'),
@@ -35,13 +36,23 @@ module.exports = {
     		{
     			test: /\.(png|svg|jpg|gif|jpeg|ico)$/,
     			use: [
-    				'file-loader'
+	    			{
+	    				loader: 'file-loader',
+	    				options: {
+	    					name: '[name]-[hash:6].[ext]'
+	    				}
+	    			}
     			]
     		},
     		{
     			test: /\.(woff|woff2|eot|ttf|otf)$/,
     			use: [
-    			'file-loader'
+    				{
+	    				loader: 'file-loader',
+	    				options: {
+	    					name: '[name]-[hash:6].[ext]'
+	    				}
+	    			}
     			]
     		}
     	]
@@ -51,5 +62,10 @@ module.exports = {
     		jQuery: 'jquery',
     		$: 'jquery',
     	}),
+    	new CopyWebpackPlugin([
+    		// copies to {outout}/static
+    		{ from: './assets/static/', to: 'static'}
+    	]),
+
     ]
 };
