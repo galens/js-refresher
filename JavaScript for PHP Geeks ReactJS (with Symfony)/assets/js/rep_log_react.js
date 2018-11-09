8a12bcc3284a36f51ae715cacabbcd1b8e17e0f6
Galen Senogles <galen.senogles@gmail.com>
2018-11-09 14:38:12 -0800

    Initial commit for new tutorial

---
+++ JavaScript for PHP Geeks ReactJS (with Symfony)/assets/js/rep_log_react.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const el = <h2>Lift Stuff! <span>h</span></h2>
+console.log(el);
+
+ReactDom.render(el, document.getElementById('lift-stuff-app'));
